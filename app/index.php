ae0424981e2b655821f6c40be95487cf11a044ed
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2021-05-11 20:00:26 -0300

    commit

--- app/index.php
+++ app/index.php
@@ -28,6 +28,8 @@ $app->group('/usuarios', function (RouteCollectorProxy $group) {
     $group->get('[/]', \UsuarioController::class . ':TraerTodos');
     $group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
     $group->post('[/]', \UsuarioController::class . ':CargarUno');
+    //modificar
+    //borrar
   });
 
 $app->get('[/]', function (Request $request, Response $response) {    
