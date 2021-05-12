01178c3556b1c940e5d15d2644a8b36fb8fce051
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2021-05-11 22:28:28 -0300

    commit

--- app/index.php
+++ app/index.php
@@ -5,6 +5,7 @@ ini_set('display_errors', 1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
 use Slim\Routing\RouteContext;
@@ -22,37 +23,42 @@ $app = AppFactory::create();
 // Add error middleware
 $app->addErrorMiddleware(true, true, true);
 
+$mw = function (Request $request, RequestHandler $handler) {
+  $response = $handler->handle($request);
+  $response->getBody()->write('World');
+
+  return $response;
+};
+/* VERSION < 4
 $mw = function ($request, $response, $next) {
   $response->getBody()->write('BEFORE');
   $response = $next($request, $response);
   $response->getBody()->write('AFTER');
 
   return $response;
-};
-
+};*/
+/*
 $VerificarUserYPass = function ($request, $response, $next) {
  
     $response->getBody()->write("Entré al MW!");
-    /*$ArrayDeParametros = $request->getParsedBody();
+    $ArrayDeParametros = $request->getParsedBody();
     var_dump($ArrayDeParametros);
     $usuario=$ArrayDeParametros['usuario'];
     $clave=$ArrayDeParametros['clave'];
 
     if($usuario=="administrador" && $clave=="1234")
     {
-      $response->getBody()->write("<h3>Bienvenido $usuario </h3>");*/
+      $response->getBody()->write("<h3>Bienvenido $usuario </h3>");
       $response = $next($request, $response);
-    //}
+    }
   return $response;  
 };
-
+*/
 // Routes
 $app->group('/usuarios', function (RouteCollectorProxy $group) {
     $group->get('[/]', \UsuarioController::class . ':TraerTodos');
     $group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
     $group->post('[/]', \UsuarioController::class . ':CargarUno');
-    //modificar
-    //borrar
   });
 
 $app->get('[/]', function (Request $request, Response $response) {    
